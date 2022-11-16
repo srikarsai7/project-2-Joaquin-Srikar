d6713e578a302bca45d5e6c9c9a39909021b530f
JoaquinArredondo <116196373+JoaquinArredondo@users.noreply.github.com>
2022-11-16 12:25:38 -0800

    Nov. 16
    CHANGED CODE

--- project2.js
+++ project2.js
@@ -21,24 +21,80 @@ let lamax = 100;
 let nr = 0;
 let spitx = 525;
 let spity = 280;
+
+let x1 = [];
+let y1 = [];
+let r = [];
+let s = [];
+let c = [];
+
+let rx = []; 
+let dy = [];
+let dx = [];
+let cx = []; 
+let numLeaf = 80; 
+
 function setup() {
   createCanvas(600, 600);
   legflag = false;
   lamaflag = false;
+
+
+   for (let i = 0 ; i < numLeaf; i++)
+    {
+      x1.push(random(width));
+      y1.push(random(height));
+     r.push(random(-2*PI,2*PI));
+      s.push(random(0.1,0.3));
+      c.push(random(0,255));
+      
+     // cx.push(random(0,255));
+      rx.push(random(-0.01,0.01));
+      dx.push(random(-10,10));
+      dy.push(random(-10,-10));
+      
+    }
+  
 }
 
 function draw() {
-  background(200);
+ background(199,199,255);
+for(let i=0; i < numLeaf; i++)
+    {
+      push();
+      translate(300,300);
+      leafCreature(x1[i],y1[i],s[i],r[i],c[i]);
+      pop();
+    }
+   updateBack();
+push();
+
+ fill(100,200,50);
+ rect(0,500,600,600);
+ pop();
   if (scene == 1){
+
+
+    
+
+
+
+
+
     drawlama(lamax,400,1)
     updatelama()
   }
   if (scene == 2){
     human(x,y,rot,rh,rhe,lh,lhe,rl,rlk,ll,llk);
+    //creature();
     updateHuman();
     spit(spitx,spity)
     updatespit()
+
   }
+
+
+
 }
 
 function updatespit(){
@@ -75,12 +131,14 @@ function spit(spitx,spity){
   pop();
 }
 
-function updatelama(){
+function updatelama()
+{
   br += dr
   fl += dr
   bl -= dr
   fr -= dr
   lamax += 1
+
   if (br >= 0.5){
     lamaflag = true
   }else if(br <= -0.5){
@@ -113,6 +171,7 @@ function drawlama(x,y,s){
   push()
   translate(x,y)
   scale(s)
+
   
   //lama
     noStroke()
@@ -122,6 +181,7 @@ function drawlama(x,y,s){
     rect(-100,-50,200,100,10)
     //lama head
     push()
+
       rotate(nr)
       rect(45,-150,50,100,20,20,0,0)
       fill(110, 90, 70)
@@ -211,10 +271,14 @@ function human(x,y,rot,rh,rhe,lh,lhe,rl,rlk,rr,rrk){
   push();
     translate(x,y)
     rotate(rot)
+    push();
+    fill(100,100,255);
     rect(-50,-100,100,200,10,10,10,10)
-  
+    pop();
+  creature();
   //left arm  
   push();
+      fill(100,100,255); //blue
       translate(-50,-85)
       rotate(lh)
       rect(0,0,20,55,10,10,10,10)
@@ -223,6 +287,7 @@ function human(x,y,rot,rh,rhe,lh,lhe,rl,rlk,rr,rrk){
         rotate(lhe)
         rect(0,0,20,65,10,10)
         push();
+        fill(250,216,186); // tan 
           translate(0,65)
           ellipse(10,10,20)
           ellipse(2,20,5,12)
@@ -237,14 +302,17 @@ function human(x,y,rot,rh,rhe,lh,lhe,rl,rlk,rr,rrk){
     // right arm
     push();
       translate(50,-85)
+      fill(100,100,255);
       rotate(rh)
       rect(0,0,20,55,10,10,10,10)
+
       push();
         translate(0,55)
         rotate(rhe)
         rect(0,0,20,65,10,10,10,10)
         push();
           translate(0,65)
+          fill(250,216,186);
           ellipse(10,10,20)
           ellipse(2,20,5,12)
           ellipse(7,20,5,12)
@@ -256,6 +324,7 @@ function human(x,y,rot,rh,rhe,lh,lhe,rl,rlk,rr,rrk){
   
     // left leg
     push();
+      fill('#402A12');
       translate(-40,100)
       rotate(rl)
       rect(0,0,20,50,5,5,5,5)
@@ -264,6 +333,7 @@ function human(x,y,rot,rh,rhe,lh,lhe,rl,rlk,rr,rrk){
         rotate(rlk)
         rect(0,0,20,50,5,5,5,5)
         push();
+          fill(0);
           translate(0,50)
           rect(0,0,20,30,10,10,10,10)
         pop();
@@ -272,6 +342,7 @@ function human(x,y,rot,rh,rhe,lh,lhe,rl,rlk,rr,rrk){
     
     // right leg
     push();
+    fill('#402A12');
       translate(20,100)
       rotate(ll)
       rect(0,0,20,50,5,5,5,5)
@@ -281,10 +352,108 @@ function human(x,y,rot,rh,rhe,lh,lhe,rl,rlk,rr,rrk){
         rect(0,0,20,50,5,5,5,5)
         push();   
           translate(0,50)
+          fill(0);
           rect(0,0,20,30,10,10,10,10)
         pop();
       pop();
     pop();
     
   pop() 
+}
+
+function creature()
+{
+  push();//FIRST PUSH
+  
+  translate(0,-150);
+  //BACK HAIR
+  push();
+  fill('#452A1D');
+  ellipse(0,-50,50);
+  ellipse(-20,-40,50);
+  ellipse(20,-40,50);
+  ellipse(30,-20,25);
+  ellipse(-30,-20,25);
+  pop();
+  //FACE
+  push();
+  fill(250,216,186);
+  noStroke();
+  ellipse(0,0,70,100);
+  ellipse(0,20,100,60);
+  pop();
+  //EYES
+  ellipse(-15,-10,10,10);
+  ellipse(15,-10,10,10);
+ 
+  
+  ellipse(-15,-10,2,2);
+  ellipse(15,-10,2,2);
+  
+  //EYEBROWS
+  push();
+  strokeWeight(3);
+  line(-30,-25,-4,-20)
+  line(30,-25,4,-20)
+  pop();
+  
+  //NOSE
+  line(0,0,10,8);
+  line(10,8,0,8);
+  //MOUTH
+  push();
+  fill(255,10,10);
+  ellipse(0,30,40,15);
+  pop();
+  rect(-10,23,10,10);
+  rect(3,23,10,10);
+ 
+  //BLUSH
+  push();
+  noStroke();
+  fill(255,200,200)
+  ellipse(30,10,20,10);
+  ellipse(-30,10,20,10)
+  pop();
+  
+  
+  
+  pop();//LAST POP
+}
+
+
+function leafCreature(x1,y1,s,r,c)
+{
+  push();
+  scale(s);
+  rotate(r);
+  translate(x1,y1);
+  fill(c,150,150);
+  ellipse(0,0,50,100);
+  line(0,0,0,100);
+  pop();
+}
+
+function updateBack ()
+{
+  for (let i = 0; i < numLeaf ; i++)
+    {
+      x1[i]=x1[i]+dx[i];
+      y1[i]=y1[i]+dy[i];
+      r[i]=r[i]+rx[i];
+      //c[i]=c[i]+cx[i];
+      
+      //BOUNDING BOX NEEDS WORK
+      
+      //BOUNDING BOX
+      if (x1[i]>width+300|| x1[i] <-300)
+          {
+            dx[i]=-dx[i];
+          }
+      if(y1[i]>height+300||y1[i]<-300)
+        {
+          dy[i]=-dy[i];
+        }
+        
+    }
 }
